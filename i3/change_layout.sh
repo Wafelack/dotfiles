59e11079c5404dfb6316e01f8ba53a94e1a9c711
Wafelack <wafelack@protonmail.com>
2021-02-18 18:48:12 +0100

    :hammer: Updated layout changer

--- i3/change_layout.sh
+++ i3/change_layout.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 current_layout=$(echo $(setxkbmap -query | grep layout) | sed "s/layout://")
 echo "'$current_layout'"
-if [[ $current_layout == " fr" ]]
+if [[ $current_layout == " fr-latin9" ]]
 then
   setxkbmap us
 else
-  setxkbmap fr
+  setxkbmap fr-latin9
 fi
