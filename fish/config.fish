681671ce8cf373467e89693f3e5e6f62ff90103d
Wafelack <wafelack@protonmail.com>
2021-02-18 15:31:18 +0100

    :sparkles: Added rofi script.

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,10 @@ function py
   python3
 end
 
+function please
+  sudo
+end
+
 function rl
   source $HOME/.config/fish/config.fish
 end
@@ -28,14 +32,13 @@ set PATH /home/wafelack/.cargo/bin $PATH
 
 # Prompt
 function fish_prompt
-  echo '╭─'(set_color red) $USER (set_color normal)'at'(set_color yellow) $hostname(set_color normal)
+  echo '╭─'(set_color green) (string replace "/home/wafelack" "~" (pwd))(set_color normal)
   set -l is_git (git rev-parse --is-inside-work-tree 2> /dev/null)
   switch $is_git
     case 'true'
       echo '->'(set_color cyan) (git branch --show-current) (set_color normal)'['(set_color yellow)(git log --format='%h' -n 1 2> /dev/null)(set_color normal)']'
   end
-
-  echo '╰─'(set_color green) (string replace "/home/wafelack" "~" (pwd))(set_color normal) '» '
+  echo '╰─ » '
 end
 
 # Variables
