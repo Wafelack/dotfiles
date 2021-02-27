11eb59b3940c03752f80c7c5e07cca6f70252e8a
Wafelack <wafelack@protonmail.com>
2021-02-27 20:06:35 +0100

    feat(fish): Updating prompt.

--- fish/config.fish
+++ fish/config.fish
@@ -35,14 +35,25 @@ set PATH /home/wafelack/.cargo/bin $PATH
 set PATH "$PREFIX/bin" $PATH
 
 # Prompt
+# function fish_prompt
+#  echo '╭─'(set_color green) (string replace "/home/wafelack" "~" (pwd))(set_color normal)
+#  set -l is_git (git rev-parse --is-inside-work-tree 2> /dev/null)
+#  switch $is_git
+#    case 'true'
+#      echo '->'(set_color cyan) (git branch --show-current) (set_color normal)'['(set_color yellow)(git log --format='%h' -n 1 2> /dev/null)(set_color normal)']'
+#  end
+#  echo (set_color normal)'╰─ » '
+# end
+
 function fish_prompt
-  echo '╭─'(set_color green) (string replace "/home/wafelack" "~" (pwd))(set_color normal)
+  echo -n (set_color green)$USER(set_color normal)'@'(set_color red)$hostname(set_color normal)':'(set_color yellow) (string replace "/home/wafelack" "~" (pwd)) (set_color normal) 
   set -l is_git (git rev-parse --is-inside-work-tree 2> /dev/null)
   switch $is_git
     case 'true'
-      echo '->'(set_color cyan) (git branch --show-current) (set_color normal)'['(set_color yellow)(git log --format='%h' -n 1 2> /dev/null)(set_color normal)']'
+      echo '|'(set_color cyan)(git branch --show-current) (set_color normal)'- '(git log --format='%h' -n 1 2> /dev/null)'| $ '
+    case '*'
+      echo (set_color normal)'$ ' 
   end
-  echo (set_color normal)'╰─ » '
 end
 
 # Variables
