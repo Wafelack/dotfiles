854b9c611e137a221b53dfdf1375147ec265c9d6
Wafelack <wafelack@protonmail.com>
2021-02-28 20:59:07 +0100

    scripts: Fixing battery script.

--- i3blocks/battery.sh
+++ i3blocks/battery.sh
@@ -8,7 +8,7 @@ if [[ $is_full == " 100%" ]]
 then
   echo "100%"
 else
-  LEVEL=echo $out | grep -o '[0-9][0-9]'
+  LEVEL=$(echo $out | grep -o '[0-9][0-9]')
 
   echo $out | grep -o '[0-9][0-9]%'
 fi
