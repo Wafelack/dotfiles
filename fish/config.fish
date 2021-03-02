fd8e8e4d5c1cec82be1bfd76e39dd8adc3bfe4e7
Wafelack <wafelack@protonmail.com>
2021-03-02 08:32:54 +0100

    shell: Added some aliases.

--- fish/config.fish
+++ fish/config.fish
@@ -2,50 +2,58 @@
 
 # aliases
 function py
-  python3
+	python3 $argv
 end
 
 function please
-  sudo
+	sudo $argv
+end
+
+function emacs
+	vim $argv
+end
+
+function rtfm
+	man $argv
 end
 
 function rl
-  source $HOME/.config/fish/config.fish
+source $HOME/.config/fish/config.fish
 end
 
 # Ssh keys management
 eval (ssh-agent -c)
-ssh-add
+	ssh-add
 
 # Gpg
 set GPG_TTY (tty)
-export GPG_TTY
+	export GPG_TTY
 
-# OS
-set PREFIX "/home/wafelack/opt/cross"
-set TARGET i686-elf
+# Cross compiler
+	set PREFIX "/home/wafelack/opt/cross"
+	set TARGET i686-elf
 
 # Path
-set PATH /home/wafelack/ $PATH
-set PATH $DENO_INSTALL/bin $PATH
-set PATH /home/wafelack/.werb_bin $PATH
-set PATH /home/wafelack/.lua $PATH
-set PATH /home/wafelack/.gem/ruby/2.7.0/bin $PATH
-set PATH /home/wafelack/.cargo/bin $PATH
-set PATH "$PREFIX/bin" $PATH
+	set PATH /home/wafelack/ $PATH
+	set PATH $DENO_INSTALL/bin $PATH
+	set PATH /home/wafelack/.werb_bin $PATH
+	set PATH /home/wafelack/.lua $PATH
+	set PATH /home/wafelack/.gem/ruby/2.7.0/bin $PATH
+	set PATH /home/wafelack/.cargo/bin $PATH
+	set PATH "$PREFIX/bin" $PATH
 
-function fish_prompt
-  set -l is_git (git rev-parse --is-inside-work-tree 2> /dev/null)
-  switch $is_git
-    case 'true'
-      echo -n '['(set_color yellow)(git branch --show-current)(set_color normal)'] '
-  end
-  echo (set_color cyan)(prompt_pwd)(set_color normal) '$ '
-end
+	function fish_prompt
+set -l is_git (git rev-parse --is-inside-work-tree 2> /dev/null)
+	switch $is_git
+	case 'true'
+	echo -n '['(set_color yellow)(git branch --show-current)(set_color normal)'] '
+	end
+	echo (set_color cyan)(prompt_pwd)(set_color normal) '$ '
+	end
 
 # Remap escape to caps lock
 
-setxkbmap -option caps:escape
+	setxkbmap -option caps:escape
 
 # Variables
-export TERM=xterm-256color
+	export TERM=xterm-256color
